99d388efd2ed005d69ad0f61bb7478c1d2d65e52
rimmachepuxa <ch4epuxa@gmail.com>
2025-09-30 14:35:14 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -1,1 +1,4 @@
+number1 = float(input("Enter first number: ")) 
+number2 = float(input("Enter second number: "))
 
+print("Both numbers are equal") if number1 == number2 else print(f"smallest number:{min(number1,number2)}") #выводит наименьшее число из двух 
