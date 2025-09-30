2e13f63bfea9406a4581824dad6fa1c8da80a649
rimmachepuxa <ch4epuxa@gmail.com>
2025-09-30 14:38:09 +0300

    Update main.py

--- task_5/main.py
+++ task_5/main.py
@@ -1,1 +1,18 @@
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
 
+if x == 0 and y == 0:
+    print("Origin") 
+elif x == 0:
+    print("Abscissa") # ось абсцисс
+elif y == 0:
+    print("Ordinate") # ось ординат 
+
+elif x > 0 and y > 0:
+    print("1 quarter") # если x, y положительны, то первая четверть 
+elif x<0 and y>0:
+    print("2 quarter") # если x отрицательный, y положительный , то вторая четверть 
+elif x<0 and y<0:
+    print("3 quarter") # если x, y отрицательны, то третья четверть 
+else:
+    print("4 quarter") # оставшийся вариант четвертая четверть 
